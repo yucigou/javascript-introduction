9aad714491915463bfbff346cd4b0f41b9f1d706
Yuci Gou <yuci.gou@gmail.com>
2018-02-09 11:29:45 +0000

    Update main.js

--- 20-project/js/main.js
+++ 20-project/js/main.js
@@ -4,7 +4,6 @@ function callWebService(url, callback) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			var results = JSON.parse(this.response);
 			callback(this.response);
 		}
 	};
